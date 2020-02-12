6748f0cbb674b8dd2ca878e6659ced8875a4144c
rabindratamang <rabindra.tamang@yarshastudio.com>
2020-02-12 22:42:51 +0545

    Update index.js with simple express app

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
-const hapi = require("@hapi/hapi");
+const express = require('express')
+const app = express()
+const port = 3000
 
-async function start() {
-  const server = hapi.server({
-    host: "0.0.0.0",
-    port: process.env.PORT || 3000
-  });
+app.get('/', (req, res) => res.send({
+  'status':true,
+  'message':'Hello World!'
+}))
 
-  server.route({
-    method: "GET",
-    path: "/",
-    handler() {
-      return { success: true, message: "here" };
-    }
-  });
-
-
-
-  
-
-  await server.register({
-    plugin: require("hapi-pino"),
-    options: {
-      prettyPrint: true
-    }
-  });
-
-  await server.start();
-
-  return server;
-}
-
-start().catch(err => {
-  console.log(err);
-  process.exit(1);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
